d54a11a6984aad0aa87cbcfc731b534293f70aeb
sesidhar31 <sesi.nandu@gmail.com>
2021-02-21 20:58:01 +0530

    second commit from terminal

--- src/seleniumPractice/BrowserInvoke.java
+++ src/seleniumPractice/BrowserInvoke.java
@@ -8,15 +8,15 @@ public class BrowserInvoke {
 
 	public static void main(String[] args) throws InterruptedException {
 
-		
+		//setting system properties
 		System.setProperty("webdriver.chrome.driver", "/home/sesidharn/Downloads/chromedriver_linux64 (1)/chromedriver");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com/");
 		String url=driver.getCurrentUrl();
-		System.out.println(url);
+		System.out.println(url); 
 		System.out.println(driver.getTitle().charAt(3));
 		System.out.println(driver.getTitle());
-		int r=driver.getTitle().indexOf("l");
+		int r=driver.getTitle().indexOf("l");//index of l
 		System.out.println(r);
 		
 		driver.findElement(By.name("q")).sendKeys("selenium");
